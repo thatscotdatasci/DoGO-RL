df70328dab8fa9c9401fa49e0368d0349b2f1c11
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-04-28 15:25:02 +0100

    Install clarity

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,9 @@
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
 
-python -m pip install --upgrade Sphinx myst-parser
+echo "Trying to do the install!!"
+python3 -m pip install --upgrade Sphinx myst-parser
+echo "Install attempted"
  
 #####################
 # DECLARE VARIABLES #
