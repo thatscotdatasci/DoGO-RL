d6916b5e12360448c48b644245b21eaa59d0ffb4
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-04-28 15:19:57 +0100

    Upgrade to newer Sphinx

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
 
-python -m pip install --upgrade myst-parser
+python -m pip install --upgrade Sphinx myst-parser
  
 #####################
 # DECLARE VARIABLES #
