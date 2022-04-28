7c1d668e93dd49109598ef37f8e2c04724dd8b8c
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-04-28 14:37:34 +0100

    Initial docs commit

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+import sphinx_rtd_theme
+
+project = 'DoGO'
+copyright = '2022, Alan Clark'
+author = 'Alan Clark'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+master_doc = 'index'
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
